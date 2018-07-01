4ee6b6e46c53bc7994e7b9095e23970abb47174c
Yuxin Zhou <cn.cindyzhou@gmail.com>
2018-07-01 12:41:06 -0700

    prod env setting

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: GOOGLE_CLIENT_ID,
+  googleClientSecret: GOOGLE_CLIENT_SECRET,
+  mongoURI: MONGO_URI,
+  cookieKey: COOKIE_KEY,
+  stripePublicKey: STRIPE_PUB_KEY,
+  stripeSecretKey: STRIPE_SECRET_KEY
+};
